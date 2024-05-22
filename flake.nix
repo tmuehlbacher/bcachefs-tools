b91647aab80701c5b462bdcdaefb2cc72821014a
Thomas Mühlbacher <tmuehlbacher@posteo.net>
2024-05-22 20:41:19 +0200

    style(nix): run `nix fmt`
    Signed-off-by: Thomas Mühlbacher <tmuehlbacher@posteo.net>

--- flake.nix
+++ flake.nix
@@ -9,18 +9,25 @@
     flake = false;
   };
 
-  outputs = { self, nixpkgs, utils, ... }:
+  outputs =
     {
-      overlays.default = final: prev: {
-        bcachefs = final.callPackage ./build.nix { };
-      };
-    } // utils.lib.eachDefaultSystem (system:
+      self,
+      nixpkgs,
+      utils,
+      ...
+    }:
+    {
+      overlays.default = final: prev: { bcachefs = final.callPackage ./build.nix { }; };
+    }
+    // utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ self.overlays.default ];
         };
-      in {
+      in
+      {
         packages = {
           inherit (pkgs) bcachefs;
           bcachefs-fuse = pkgs.bcachefs.override { fuseSupport = true; };
@@ -29,12 +36,31 @@
 
         formatter = pkgs.nixfmt-rfc-style;
 
-        devShells.default = pkgs.callPackage ({ mkShell, rustc, cargo, gnumake
-          , gcc, clang, pkg-config, libuuid, libsodium, keyutils, liburcu, zlib
-          , libaio, zstd, lz4, udev, bcachefs }:
+        devShells.default = pkgs.callPackage (
+          {
+            mkShell,
+            rustc,
+            cargo,
+            gnumake,
+            gcc,
+            clang,
+            pkg-config,
+            libuuid,
+            libsodium,
+            keyutils,
+            liburcu,
+            zlib,
+            libaio,
+            zstd,
+            lz4,
+            udev,
+            bcachefs,
+          }:
           mkShell {
             LIBCLANG_PATH = "${clang.cc.lib}/lib";
             inherit (bcachefs) nativeBuildInputs buildInputs;
-          }) { };
-      });
+          }
+        ) { };
+      }
+    );
 }
