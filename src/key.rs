tmuehlbacher/bcachefs-tools

commit f72ded6a4f80117b794236018ba301ae30e2d93d

fix(key): search for key in all relevant keyrings
Previously, using `bcachefs unlock -k session` would still cause mount
to ask for a passphrase.

Signed-off-by: Thomas Mühlbacher <tmuehlbacher@posteo.net>

src/key.rs
@@ -110,23 +110,30 @@ impl KeyHandle {
         }
     }
 
-    pub fn new_from_search(uuid: &Uuid) -> Result<Self> {
-        let key_name = Self::format_key_name(uuid);
-        let key_name = CStr::as_ptr(&key_name);
+    fn search_keyring(keyring: i32, key_name: &CStr) -> Result<i64> {
+        let key_name = CStr::as_ptr(key_name);
         let key_type = c_str!("user");
 
-        let key_id =
-            unsafe { keyctl_search(keyutils::KEY_SPEC_USER_KEYRING, key_type, key_name, 0) };
+        let key_id = unsafe { keyctl_search(keyring, key_type, key_name, 0) };
 
         if key_id > 0 {
             info!("Found key in keyring");
-            Ok(Self {
-                _uuid: *uuid,
-                _id:   key_id,
-            })
+            Ok(key_id)
         } else {
             Err(ErrnoError(errno::errno()).into())
         }
+    }
+
+    pub fn new_from_search(uuid: &Uuid) -> Result<Self> {
+        let key_name = Self::format_key_name(uuid);
+
+        Self::search_keyring(keyutils::KEY_SPEC_SESSION_KEYRING, &key_name)
+            .or_else(|_| Self::search_keyring(keyutils::KEY_SPEC_USER_KEYRING, &key_name))
+            .or_else(|_| Self::search_keyring(keyutils::KEY_SPEC_USER_SESSION_KEYRING, &key_name))
+            .map(|id| Self {
+                _uuid: *uuid,
+                _id:   id,
+            })
     }
 
     fn wait_for_unlock(uuid: &Uuid) -> Result<Self> {

commit 411df4e53fdc58cfca2e473e54c1bed9cf17740d

key: use c_long type for keyctl_search() helper fn
The keyctl_search() C function returns a long, which is already
reflected in the KeyHandle._id type. The search_keyring() helper
function currently returns a Result<i64>, which breaks 32-bit builds for
e.g. armv7l:
  error[E0308]: mismatched types
     --> src/key.rs:121:16
      |
  121 |             Ok(key_id)
      |             -- ^^^^^^ expected `i64`, found `i32`
      |             |
      |             arguments to this enum variant are incorrect
...
  error[E0308]: mismatched types
     --> src/key.rs:135:24
      |
  135 |                 _id:   id,
      |                        ^^ expected `i32`, found `i64`

Fix this by changing search_keyring() to return a Result<c_long>.

Fixes: f72ded6a ("fix(key): search for key in all relevant keyrings")
Signed-off-by: David Disseldorp <ddiss@suse.de>
Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

src/key.rs
@@ -110,7 +110,7 @@ impl KeyHandle {
         }
     }
 
-    fn search_keyring(keyring: i32, key_name: &CStr) -> Result<i64> {
+    fn search_keyring(keyring: i32, key_name: &CStr) -> Result<c_long> {
         let key_name = CStr::as_ptr(key_name);
         let key_type = c_str!("user");
 