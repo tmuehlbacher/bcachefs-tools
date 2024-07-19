09cde4869dec78c520333834c94bf929047ca839
Thomas Mühlbacher <tmuehlbacher@posteo.net>
2024-07-19 21:34:15 +0200

    feat(logging): switch to custom file:line format
    This mostly tries to be similar to the default `env_logger` format but
    instead of using the more vague target in the log message, we instead
    put the file name and line number in the log.
    
    Signed-off-by: Thomas Mühlbacher <tmuehlbacher@posteo.net>

--- src/logging.rs
+++ src/logging.rs
@@ -1,5 +1,8 @@
+use std::io::Write;
+
 use env_logger::WriteStyle;
-use log::LevelFilter;
+use log::{Level, LevelFilter};
+use owo_colors::{OwoColorize, Style};
 
 pub fn setup(verbose: u8, color: bool) {
     let level_filter = match verbose {
@@ -19,5 +22,27 @@ pub fn setup(verbose: u8, color: bool) {
         .filter_level(level_filter)
         .write_style(style)
         .parse_env("BCACHEFS_LOG")
+        .format(move |buf, record| {
+            let style = if style == WriteStyle::Never {
+                Style::new()
+            } else {
+                match record.level() {
+                    Level::Trace => Style::new().cyan(),
+                    Level::Debug => Style::new().blue(),
+                    Level::Info => Style::new().green(),
+                    Level::Warn => Style::new().yellow(),
+                    Level::Error => Style::new().red().bold(),
+                }
+            };
+
+            writeln!(
+                buf,
+                "[{:<5} {}:{}] {}",
+                record.level().style(style),
+                record.file().unwrap(),
+                record.line().unwrap(),
+                record.args()
+            )
+        })
         .init();
 }
