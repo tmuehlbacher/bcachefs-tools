017c3cc66e9900ef2ce93584f78d26adbba7e191
Thomas Mühlbacher <tmuehlbacher@posteo.net>
2025-03-21 16:33:56 +0000

    fix(nix): rm input from crane that no longer exists
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- flake.nix
+++ flake.nix
@@ -11,10 +11,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
 
     fenix = {
       url = "github:nix-community/fenix";
